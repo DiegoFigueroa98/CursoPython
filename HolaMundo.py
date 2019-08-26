155c4a9102e413a4ec916272bff462ae03e7df31
Diego Aaron Figueroa Campos <16460315@itcolima.edu.mx>
2019-08-26 10:04:34 -0500

    Primera clase de Python 26/08/2019

---
+++ HolaMundo.py
@@ -0,0 +1,6 @@
+a = int(input("Ingrese un número entero: "))
+b = int(input("Ingrese un número entero: "))
+c = a + b
+print(f"El resultado de la suma de {a} más {b} es: {c}")
+
+
---
+++ Sentencia_If.py
@@ -0,0 +1,16 @@
+a = int(input("Ingrese un número entero: "))
+b = int(input("Ingrese un número entero: "))
+c = a + b
+print(f"\nEl resultado de la suma de {a} más {b} es: {c}")
+
+if a>b:
+    print(f"\n{a} es mayor que {b}")
+elif a==b:
+    print(f"\n{a} es igual que {b}")
+else:
+    print(f"\n{a} es menor que {b}")
+
+print("\nFin del programa")
+
+
+
